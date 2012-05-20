f3d4795e4f4822dd8f99f87f32e40a29f6b135f8
David Huber <david.huber@gmail.com>
2012-05-20 14:45:38 -0500

    Implemented guessing game.

--- ruby/day_1/guess.rb
+++ ruby/day_1/guess.rb
@@ -4,7 +4,7 @@ secret = Random.new().rand(1..10)
 until guess == secret
   direction = guess && guess > secret ? 'high' : 'low'
   puts "Too %{direction}!" % {:direction => direction} unless guess == nil
-  puts "Guess a number!"
+  puts "Guess a number between 1 and 10!"
   guess = (gets).to_i
 end
 
