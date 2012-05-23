cc5a2591521b4b4f75a9c7fb3a20ee1b429f8174
David Huber <david.huber@gmail.com>
2012-05-23 17:10:07 -0500

    hey I'm a bit better at string formatting now.

--- ruby/day_1/guess.rb
+++ ruby/day_1/guess.rb
@@ -3,7 +3,7 @@ secret = Random.new().rand(1..10)
 
 until guess == secret
   direction = guess && guess > secret ? 'high' : 'low'
-  puts "Too %{direction}!" % {:direction => direction} unless guess == nil
+  puts "Too #{direction}!" unless guess == nil
   puts "Guess a number between 1 and 10!"
   guess = (gets).to_i
 end
